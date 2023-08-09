82bc7fd01ed4d1ba712e253a1db87dd6459ec53e
abiy17 <abiygetahun17@gmail.com>
2023-08-09 22:14:08 +0300

    commit message

--- vite-project/src/components/Rating.jsx
+++ vite-project/src/components/Rating.jsx
@@ -6,7 +6,7 @@ function Rating() {
     const [Rating,setRating] = useState(0)
     const [feedback,setfeedback] = useState("")
     const [hover,sethover] = useState(null)
-    const { RatingCpt,setRatingCpt } = useContext(MyContext)
+    const { RatingCpt,setRatingCpt,mode,setmode } = useContext(MyContext)
     const HandleRating=(e)=>{
         e.preventDefault();
         axios.post(`http://localhost:5000/rating`,{ Rating,feedback })
@@ -16,11 +16,11 @@ function Rating() {
         setfeedback("")
     }   
     return (
-    <form onSubmit={HandleRating}>
-        <div className={RatingCpt ? "flex flex-col gap-10 p-5 w-4/6 items-center bg-stone-50 rounded-xl min-h-[2oem] m-auto" : "hidden"}>
+    <form className={RatingCpt ? "block" : "hidden"} onSubmit={HandleRating}>
+        <div className={mode ? "flex flex-col gap-10 p-5 w-4/6 items-center bg-gray-950 rounded-xl min-h-[2oem] m-auto" : "flex flex-col gap-10 p-5 w-4/6 items-center bg-stone-50 rounded-xl min-h-[2oem] m-auto"}>
             <div className="flex pt-4">
                 <p className="text-slate-700 font-bold">How did we do?</p>
-                <FaWindowClose onClick={()=>setRatingCpt(false)} className="relative left-56 cursor-pointer"/>
+                <FaWindowClose onClick={()=>setRatingCpt(false)} className={mode ? "relative left-56 cursor-pointer text-white" : "relative left-56 cursor-pointer"}/>
             </div>
             <p className="text-[1em] w-[30em] text-center text-slate-500">Please let us know how we did with your support request. All feedback
           is appreciated to help us improve our offering!</p>
@@ -29,7 +29,7 @@ function Rating() {
                 const ratingCount = i + 1
                 return <div className="cursor-pointer">
                     <input type="radio" name="rating" value={ratingCount} onClick={()=>{setRating(ratingCount)}} className="hidden"/>
-                    <FaStar 
+                    <FaStar
                     onClick={()=>{setRating(ratingCount)}}
                     size={50}
                     color={ratingCount <= (hover || Rating) ? "#ffc107" : "#e4e5e9"}
@@ -41,7 +41,7 @@ function Rating() {
             </div>
             <label htmlFor="input" className="flex flex-col gap-3 relative top-[-1em]">
                 <p></p>
-                <input type="text" className="w-[29em] pb-10 outline-1 outline-orange-300 rounded-lg p-5 text-[.7em]" value={feedback} placeholder="give us feedback here..." required onChange={(e)=>{setfeedback(e.target.value)}}/>
+                <input type="text" className={mode ? "w-[29em] pb-10 outline-1 bg-gray-900 outline-slate-900 rounded-lg p-5 text-[.7em]" : "w-[29em] pb-10 outline-1 outline-orange-300 rounded-lg p-5 text-[.7em]"} value={feedback} placeholder="give us feedback here..." required onChange={(e)=>{setfeedback(e.target.value)}}/>
                 <button type="submit" className="w-[20.4em] p-2 hover:opacity-80 duration-200 active:opacity-70 m-auto bg-orange-500 text-white rounded-md">submit</button>
             </label>
         </div>
